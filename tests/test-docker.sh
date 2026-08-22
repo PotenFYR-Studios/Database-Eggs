d078590283daf3e98aea29f1235af93a3bbfc2dc
Ded3ec <ded3ec@proton.me>
2026-08-22 19:58:50 +0530

    fix(ci): define C_MAGENTA and C_DIM in test-docker.sh to fix unbound variable error

--- tests/test-docker.sh
+++ tests/test-docker.sh
@@ -16,6 +16,8 @@ C_RED='\033[31m'
 C_YELLOW='\033[33m'
 C_CYAN='\033[36m'
 C_BLUE='\033[34m'
+C_MAGENTA='\033[35m'
+C_DIM='\033[2m'
 
 IMAGE_NAME="${IMAGE_NAME:-database-eggs:test}"
 BUILD_IMAGE="${BUILD_IMAGE:-1}"
